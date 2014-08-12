3dafb47ba84cfd73cc8f2448033e6649d4154061
foyo23 <foyo23@gmail.com>
2014-08-12 10:38:41 +0000

    fix for 'no command make'

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM centos:centos7
 MAINTAINER foyo23 <foyo23@gmail.com>
 
 ADD install_ruby.sh /tmp/
-RUN /tmp/install_ruby.sh
+RUN yum -y update && \
+    /tmp/install_ruby.sh
 
 RUN gem update --system --no-document
 RUN gem install bundler --no-ri --no-rdoc
--- install_ruby.sh
+++ install_ruby.sh
@@ -8,6 +8,16 @@ cd /tmp
 yum list installed | cut -f 1 -d " " | uniq | sort > /tmp/pre
 yum install bzip2 git tar wget -y
 
+wget http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm && \
+    rpm -ivh epel-release-7-0.2.noarch.rpm && \
+    yum -y update && \
+    yum -y install sudo nginx git-core python zlib-devel \
+       readline-devel ncurses-devel gdbm-devel glibc-devel tcl-devel openssl-devel curl-devel expat-devel db4-devel \
+       byacc gcc-c++ libyaml-devel libffi-devel libxml2-devel libxslt-devel libicu-devel system-config-firewall-tui \
+       python-devel make cmake bison libodb-mysql-devel patch libtool libicu-devel && \
+    gem install --no-ri --no-rdoc bundler && \
+    yum clean all
+
 wget -O ruby-install-$RUBY_INSTALL_VERSION.tar.gz \
   https://github.com/postmodern/ruby-install/archive/v$RUBY_INSTALL_VERSION.tar.gz
 tar -xzf ruby-install-$RUBY_INSTALL_VERSION.tar.gz
